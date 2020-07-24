7f1fac1822538d0a604ea996723f33215af6cedf
CHJtlqd <tlqd123@outlook.com>
2020-07-24 10:04:18 +0900

    20200723

---
+++ Delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+
+    class Program
+    {
+        delegate int NewType(int Num);
+
+        static int OnePlus(int Num)
+        {
+            return Num + 1;
+        }
+
+        static int TenPlus(int Num)
+        {
+            return Num + 10;
+        }
+        static void Main(string[] args)
+        {
+            NewType aNewType = new NewType(OnePlus);
+            //Console.WriteLine(aNewType(1));
+
+            //aNewType = new NewType(TenPlus);
+            aNewType += TenPlus;
+            Console.WriteLine(aNewType(1000));
+        }
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Program
+    {
+        delegate int Calculator(int num1, int num2);
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        static int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+
+        static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        static void Main(string[] args)
+        {
+            int iResult;
+            Calculator[] aCalculator = new Calculator[] { Add, Sub, Mul, Div };
+            for (int i = 0; i < aCalculator.Length; i++)
+            {
+                Console.WriteLine(aCalculator[i](3, 4));
+            }
+
+            foreach (var item in aCalculator)
+            {
+                Console.WriteLine(item(3, 4));
+            }
+
+            iResult = Add(3, 4);
+            Console.WriteLine(iResult);
+
+            iResult = Div(3, 4);
+            Console.WriteLine(iResult);
+
+            iResult = Sub(3, 4);
+            Console.WriteLine(iResult);
+
+            iResult = Mul(3, 4);
+            Console.WriteLine(iResult);
+        }
+    }
+}
